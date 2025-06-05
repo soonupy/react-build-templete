1cf0f140f16057eac50b3f2c143b6dcd201f922a
soonupy <soonupy97@cadian.com>
2025-06-05 16:26:32 +0900

    react build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import { FaReact } from "react-icons/fa";
+import { ChevronDown } from 'lucide-react';
+
 
 const Header = () => {
   return (
@@ -11,7 +13,7 @@ const Header = () => {
      </Link>
      </h1>
       <nav className="header-nav">
-        <Link className="menu" to="/menu1">Menu1</Link>
+        <Link className="menu" to="/menu1">Menu1<ChevronDown size={20} /></Link>
         <Link className="menu" to="/menu2">Menu2</Link>
         <Link className="menu" to="/menu3">Menu3</Link>
         <Link className="menu" to="/menu4">Menu4</Link>
